25f04f49ba058823900336739022fedaeb17cb04
heiscostly <ayangcostly@gmail.com>
2021-12-06 18:48:16 +0100

    working on the placedetails page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/header";
 import List from "./components/List/list";
 import PlaceDetail from "./components/PlaceDetails/placeDetails";
 import Map from "./components/Map/map";
 
+import { getPlaceData } from "./api";
+
 export default function App() {
+  const [places, setPlaces] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  });
+
+  useEffect(() => {
+    console.log(coordinates, bounds, "hey guys");
+    getPlaceData(bounds?.ne, bounds?.sw).then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
+
   return (
     <>
       <CssBaseline />
@@ -15,7 +38,12 @@ export default function App() {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
+          console.log(bounds, 'helloo')
         </Grid>
       </Grid>
     </>
--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -6,9 +6,9 @@ import Rating from '@material-ui/lab'
 
 import useStyles from './styles'
 
-const coordinates = { lat: 0, lng: 0 }
+ 
 
-function Map() {
+function Map({setCoordinates, setBounds, coordinates}) {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)')
     return (
@@ -20,7 +20,10 @@ function Map() {
                 defaultZoom={14}
                 margin={[50,50,50,50]}
                 options= {""}
-                onChange={""}
+                onChange={(e) => {
+                    setCoordinates({lat: e.center.lat, lng: e.center.lng})
+                    setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+                }}
                 onChildClick={""}
             >
 
